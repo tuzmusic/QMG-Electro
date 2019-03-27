e35afdd0f3a61ac1dffbc170c9afa869461eedc0
tuzmusic <tuzmusic@gmail.com>
2019-03-27 15:39:20 -0400

    render basic map screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,6 +6,7 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import MapScreen from "../js/MapView";
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -53,7 +54,12 @@ SettingsStack.navigationOptions = {
   ),
 };
 
+const MapStack = createStackNavigator({
+  Maps: MapScreen
+})
+
 export default createBottomTabNavigator({
+  MapStack,
   HomeStack,
   LinksStack,
   SettingsStack,
