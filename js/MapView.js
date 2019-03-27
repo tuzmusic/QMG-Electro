a5370d4a77aa4b5ee2bc30278a08234ac42e1e64
tuzmusic <tuzmusic@gmail.com>
2019-03-27 15:58:37 -0400

    link region to state

--- js/MapView.js
+++ js/MapView.js
@@ -1,17 +1,30 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import MapView from "react-native-maps";
 
 export default class MapScreen extends Component {
+  state = this.getInitialState()
+  
+  getInitialState() {
+    return {
+      region: {
+        latitude: 43.208552,
+        longitude: -71.542526,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({region})
+  } 
+
   render() {
     return (
       <MapView
         style={{ flex: 1 }}
-        region={{
-          latitude: 42.882004,
-          longitude: 74.582748,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange.bind(this)}
         showsUserLocation={true}
       />
     );
