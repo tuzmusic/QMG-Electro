be10ff07026821f76a040760ac9f2c1eb1dd2d6c
tuzmusic <tuzmusic@gmail.com>
2019-03-29 12:52:37 -0400

    add array padding for design

--- js/MapResultsView.js
+++ js/MapResultsView.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import { Text, Touchable, FlatList } from "react-native";
 
 export default class MapResultsView extends Component {
+  constructor(props) {
+    super(props);
+    this.results = this.props.navigation.state.params.results;
+
+    if (this.results.length === 1) {
+      this.results = Array(5).fill(this.results[0])
+    }
+  }
 
   renderItem = ({ item }) => {
     return <Text>{item.formatted_address}</Text>;
@@ -14,7 +22,7 @@ export default class MapResultsView extends Component {
   render() {
     return (
       <FlatList
-        data={this.props.navigation.state.params.results}
+        data={this.results}
         keyExtractor={this.keyExtractor}
         renderItem={this.renderItem}
       />
