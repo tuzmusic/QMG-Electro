63500c0ffa0fdef0a0cefdc1180ab082fbd38b2b
tuzmusic <tuzmusic@gmail.com>
2019-04-09 16:45:43 -0400

    add loading modal to login screen

--- js/LoginView.js
+++ js/LoginView.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
-import { Input, Button, Image, ThemeProvider } from "react-native-elements";
-import { View } from "react-native";
+import {
+  Input,
+  Button,
+  Image,
+  ThemeProvider,
+  Overlay
+} from "react-native-elements";
+import { View, ActivityIndicator, Text } from "react-native";
 import { connect } from "react-redux";
 import { login } from "../actions/authActions";
 import F8StyleSheet from "./F8StyleSheet";
@@ -17,9 +23,21 @@ class LoginView extends Component {
 
   render() {
     console.log("isLoading =", this.props.isLoading);
-  
     return (
       <View style={styles.container}>
+        <Overlay
+          containerStyle={styles.modal}
+          height={200}
+          width={200}
+          isVisible={this.props.isLoading || true}
+          style={styles.modal}
+          borderRadius={20}
+        >
+          <View style={styles.modalContainer}>
+            <ActivityIndicator size="large" color="#0000ff" />
+            <Text>Logging in...</Text>
+          </View>
+        </Overlay>
         <ThemeProvider theme={theme}>
           <Input
             placeholder="Username"
@@ -61,7 +79,7 @@ const theme = {
   Button: {
     containerStyle: {
       padding: 30,
-      width: '100%'
+      width: "100%"
     }
   }
 };
@@ -71,6 +89,18 @@ const styles = F8StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    margin: 20
+    padding: 20,
+    backgroundColor: "lightblue"
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 40
+  },
+  modal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
