f4fce46b4f1209011eeb2e302fd87fca38bf2a35
tuzmusic <tuzmusic@gmail.com>
2019-04-09 15:46:44 -0400

    add styling using ThemeProvider

--- js/LoginView.js
+++ js/LoginView.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Input, Button, Image } from "react-native-elements";
+import { Input, Button, Image, ThemeProvider } from "react-native-elements";
 import { Text, View } from "react-native";
 import { connect } from "react-redux";
 import F8StyleSheet from "./F8StyleSheet";
@@ -13,24 +13,24 @@ class LoginView extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Input
-          // containerStyle={{ padding: 10 }}
-          placeholder="Username"
-          value={this.state.username}
-          onChangeText={username => {
-            this.setState({ username });
-          }}
-        />
-        <Input
-          // containerStyle={{ padding: 10 }}
-          placeholder="Password"
-          secureTextEntry={true}
-          value={this.state.password}
-          onChangeText={password => {
-            this.setState({ password });
-          }}
-        />
-        <Button title="Login" />
+        <ThemeProvider theme={theme}>
+          <Input
+            placeholder="Username"
+            value={this.state.username}
+            onChangeText={username => {
+              this.setState({ username });
+            }}
+          />
+          <Input
+            placeholder="Password"
+            secureTextEntry={true}
+            value={this.state.password}
+            onChangeText={password => {
+              this.setState({ password });
+            }}
+          />
+          <Button title="Login" />
+        </ThemeProvider>
       </View>
     );
   }
@@ -38,11 +38,24 @@ class LoginView extends Component {
 
 export default connect()(LoginView);
 
-styles = F8StyleSheet.create({
+const theme = {
+  Input: {
+    containerStyle: {
+      padding: 10
+    }
+  },
+  Button: {
+    containerStyle: {
+      padding: 30
+    }
+  }
+};
+
+const styles = F8StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    margin: 10,
+    margin: 20
   }
 });
