0daaf7f8f67f4f26ce1df6f8d44808cd8390d0f3
tuzmusic <tuzmusic@gmail.com>
2019-04-09 15:35:28 -0400

    control components in login form

--- js/LoginView.js
+++ js/LoginView.js
@@ -5,12 +5,32 @@ import { connect } from "react-redux";
 import F8StyleSheet from "./F8StyleSheet";
 
 class LoginView extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Input containerStyle={{ padding: 10 }} placeholder="Email" />
-        <Input containerStyle={{ padding: 10 }} placeholder="Password" />
-        <Button title='Login'/>
+        <Input
+          containerStyle={{ padding: 10 }}
+          placeholder="Username"
+          value={this.state.username}
+          onChangeText={username => {
+            this.setState({ username });
+          }}
+        />
+        <Input
+          containerStyle={{ padding: 10 }}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={this.state.password}
+          onChangeText={password => {
+            this.setState({ password });
+          }}
+        />
+        <Button title="Login" />
       </View>
     );
   }
---
+++ tests/mocks/dyson-server/users.js
@@ -0,0 +1,15 @@
+module.exports = {
+  path: "/users",
+  template: [
+    {
+      id: 1,
+      username: "sample1",
+      password: "1234567"
+    },
+    {
+      id: 2,
+      username: "sample2",
+      password: "1234567"
+    }
+  ]
+};
