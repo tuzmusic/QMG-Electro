f33bd35fb76963b3d5711b88dd6e401f5d107329
tuzmusic <tuzmusic@gmail.com>
2019-04-16 12:25:08 -0400

    skip auth

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -3,9 +3,8 @@ import MainTabNavigator from "./MainTabNavigator";
 import AuthStack from "./AuthNavigator";
 
 const AppNavigator = createSwitchNavigator({
-  Auth: AuthStack,
+  // Auth: AuthStack,
   Main: MainTabNavigator // "This screen renders a navigator!"
 });
 
 export default createAppContainer(AppNavigator);
-// export default AppNavigator;
