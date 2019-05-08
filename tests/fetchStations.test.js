49f62408cef3e750ee65b06dcdff7638da765117
tuzmusic <tuzmusic@gmail.com>
2019-05-08 16:05:05 -0400

    delete old tests

--- tests/fetchStations.test.js
+++ tests/fetchStations.test.js
@@ -36,26 +36,6 @@ describe("async fetching actions", () => {
 
   // #endregion
 
-  describe("_getImageURLForStation (private method)", () => {
-    it("returns an action with the updated station", async () => {
-      const returnedAction = await actions._getImageURLForStation(
-        firstStationObject
-      );
-      expect(returnedAction.type).toEqual("UPDATE_STATION");
-      expect(returnedAction.station.imageURL).toEqual(imageURL);
-    });
-  });
-
-  describe("getImageURLForStation (public method)", () => {
-    xit("dispatches an updated station", async () => {
-      const expectedUpdateAction = {
-        type: "UPDATE_STATION",
-        station: { ...firstStationObject, imageURL }
-      };
-      await store.dispatch(actions.getImageURLForStation(firstStationObject));
-      expect(store.getActions()).toEqual([expectedUpdateAction]);
-    });
-  });
 
   describe("fetchStations(shouldDownload)", () => {
     // #region SET UP
@@ -79,28 +59,12 @@ describe("async fetching actions", () => {
     });
     // #endregion
 
-    xit("calls _getImageURLsForAllStations", () => {
-      actions._getImageURLsForAllStations = jest.fn();
-      expect(actions._getImageURLsForAllStations).toBeCalled();
-    });
-
     it("gets stations from the website and dispatches them to the store", () => {
       expect(store.getActions()).toEqual(
         expect.arrayContaining(expectedGetActions)
       );
     });
 
-    it("updates the stations' imageURL via dispatch", () => {
-      const types = store.getActions().map(a => a.type);
-      expect(types).toEqual(expect.arrayContaining(["UPDATE_STATION"]));
-    });
-
-    xit("dispatches the update actions AFTER the get actions", () => {
-      const storeActions = store.getActions();
-      expect(storeActions.indexOf(expectedGetActions[0])).toBeLessThan(
-        storeActions.indexOf(expectedUpdateAction)
-      );
-    });
   });
 
   describe("fetchStations(useCache)", () => {
