6f02953a384f36f9c990763c2b634a7a390576d1
Jonathan Tuzman <tuzmusic@gmail.com>
2019-05-10 14:33:52 -0400

    add non-working search bar

--- src/screens/MapView.js
+++ src/screens/MapView.js
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
 import { MapView } from "expo";
-import { View, Button, Text, Platform } from "react-native";
+import { View, Button, Text, Platform, TextInput } from "react-native";
 import { BLText } from "../components/StyledComponents";
+import TabBarIcon from "../components/TabBarIcon";
 import F8StyleSheet from "../components/F8StyleSheet";
 import { connect } from "react-redux";
 const { Marker, Callout } = MapView;
 import { getLocationAsync } from "../redux/actions/userActions";
 import ListingCellView from "../subviews/ListingCellView";
 
-FindMeButton = ({ onPress }) => {
+const FindMeButton = ({ onPress }) => {
   return (
     <Callout style={styles.locationButtonCallout}>
-      <Button
-        onPress={() => {
-          console.log("Pressed");
-          onPress();
-        }}
-        title={"Find Me"}
-        style={styles.locationButton}
+      <Button onPress={onPress} title={"Find Me"} style={styles.locationButton}>
+        {/* <TabBarIcon name={"location-arrow"} library={"FontAwesome"} /> */}
+      </Button>
+    </Callout>
+  );
+};
+
+const SearchBar = props => {
+  return (
+    <Callout style={styles.searchCallout}>
+      <TextInput
+        // onChangeText={searchText => this.setState({ searchText })}
+        // onSubmitEditing={this.handleSearch.bind(this)}
+        style={styles.calloutSearch}
+        placeholder={"Search"}
+        value={props.value}
       />
     </Callout>
   );
@@ -26,6 +36,10 @@ FindMeButton = ({ onPress }) => {
 class MapScreen extends Component {
   static navigationOptions = {
     title: "Nearby Stations"
+  };
+
+  state = {
+    searchText: ""
   };
 
   renderMarkers() {
@@ -68,6 +82,10 @@ class MapScreen extends Component {
         >
           {this.renderMarkers()}
         </MapView>
+        <SearchBar
+          // onChangeText={searchText => this.setState({ searchText })}
+          value={this.state.searchText}
+        />
         <FindMeButton onPress={this.props.getLocationAsync} />
       </View>
     );
@@ -87,11 +105,23 @@ export default connect(
 )(MapScreen);
 
 const styles = F8StyleSheet.create({
+  searchCallout: {
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 15,
+    width: "90%",
+    top: 30
+  },
+  calloutSearch: {
+    // width: "90%",
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40
+  },
   locationButtonCallout: {
     borderRadius: 10,
     opacity: 0.7,
     backgroundColor: "lightgrey",
-    bottom:0,
+    bottom: 0,
     right: 0,
     margin: 10
   },
@@ -103,6 +133,6 @@ const styles = F8StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    justifyContent: "center"
+    justifyContent: "center",
   }
 });
