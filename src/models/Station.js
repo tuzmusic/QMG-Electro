e80b10c6e9310829c2331da29dff24eeab8722ab
tuzmusic <tuzmusic@gmail.com>
2019-05-15 15:36:54 -0400

    write distance-measuring methods

--- src/models/Station.js
+++ src/models/Station.js
@@ -6,12 +6,39 @@ String.prototype.stripHtmlTags = () => {
   return div.texContent || div.innerText || "";
 };
 
+export function distanceBetween(location1, location2, unit = "mi") {
+  if (location1.lat == location2.lat && location1.lng == location2.lng) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * location1.lat) / 180;
+    var radlat2 = (Math.PI * location2.lat) / 180;
+    var theta = location1.lng - location2.lng;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "km") {
+      dist = dist * 1.609344;
+    }
+    if (unit == "nm") {
+      dist = dist * 0.8684;
+    }
+    return Number(dist.toFixed(2));
+  }
+}
+
 export default class Station {
   constructor(json) {
-    if (!json) return
-    this.id =  json.id || uuid.v1()
-    this.originalJSON = json
-    this.userID = json.userID
+    if (!json) return;
+    this.id = json.id || uuid.v1();
+    this.originalJSON = json;
+    this.userID = json.userID;
     this.title = json.title;
     this.address = json.address;
     this.contactEmail = json.contactEmail;
@@ -26,6 +53,15 @@ export default class Station {
     this.imageURL = json.imageURL;
     // this.amenities = json.amenities;
   }
+
+  distanceFromLocation = (location, unit = "mi") => {
+    return distanceBetween(this.location, location);
+  };
+
+  distanceFrom = (otherStation, unit = "mi") => {
+    // unit: "mi" miles, "km", "nm" nautical miles
+    return distanceBetween(this.location, otherStation.location);
+  };
 
   static createFromApiResponse(json) {
     function p(propName, prefix = "_") {
--- tests/stationRadius.test.js
+++ tests/stationRadius.test.js
@@ -1,3 +1,4 @@
+import Station, { distanceBetween } from "../src/models/Station";
 import CupertinoStations from "./__mocks__/CupertinoStations";
 import reducer from "../src/redux/reducers/mainReducer";
 
@@ -7,5 +8,28 @@ describe("mocking stations into reducer", () => {
 
   it("can take the mock file as stations", () => {
     expect(Object.keys(returnedState.stations)[0]).toEqual("1"); // testing the testing
+  });
+});
+
+describe("Station.distanceFrom", () => {
+  const station1 = new Station(CupertinoStations[1]); // De Anza
+  const station2 = new Station(CupertinoStations[4]); // Google HQ
+  const location = CupertinoStations[4].location; // Google HQ
+
+  it("distanceBetween returns the distance between two locations", () => {
+    expect(
+      distanceBetween(
+        CupertinoStations[1].location,
+        CupertinoStations[4].location
+      )
+    ).toEqual(7.21);
+  });
+
+  it("Station.Prototype.distanceFrom(station) returns the distance between two stations, in miles", () => {
+    expect(station1.distanceFrom(station2)).toEqual(7.21);
+  });
+
+  it("Station.Prototype.distanceFromLocation(location) returns the distance between a station and a location, in miles", () => {
+    expect(station1.distanceFromLocation(location)).toEqual(7.21);
   });
 });
