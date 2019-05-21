f668adc11f6362586ff496ceb2563dc8bb262458
Jonathan Tuzman <tuzmusic@gmail.com>
2019-05-21 18:59:20 -0400

    make welcome screen a scrollview

--- src/screens/DevelopmentWelcomeView.js
+++ src/screens/DevelopmentWelcomeView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, ScrollView, Button } from "react-native";
 import { ThemeProvider } from "react-native-elements";
 
 const styles = {
@@ -12,18 +12,21 @@ const styles = {
 };
 
 const WelcomeView = props => (
-  <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+  <ScrollView
+    contentContainerStyle={{ flex: 1, justifyContent: "center", padding: 10 }}
+  >
     <Text style={{ textAlign: "center", fontSize: 24 }}>
       Welcome to the latest version of ELECTRO
     </Text>
     <Text style={styles.text}>
       The Login screen is just a dummy implementation. Enter a new username, or
       select a previous username if you've made one. This doesn't connect to any
-      database (or, of course, authenticate with a password.)
+      database (or, of course, authenticate with a password). Login will always
+      succeed.
     </Text>
     <Text style={styles.text}>
-      This development edition downloads stations from joinelectro.com, plus
-      some mock stations located around Apple HQ in Cupertino, CA.
+      This development edition downloads stations from joinelectro.com, and also
+      includes some mock stations located around Apple HQ in Cupertino, CA.
     </Text>
     <Text style={styles.text}>
       You can hit the Cupertino button in the lower-right corner of the map to
@@ -36,8 +39,8 @@ const WelcomeView = props => (
       manually)
     </Text>
     <Text style={[styles.text, styles.bug]}>
-      Pressing a station on the map will not move the station's marker into
-      view.
+      Pressing a station on the map will not automatically move the station's
+      marker into ScrollView.
     </Text>
     <Text style={[styles.text, styles.bug]}>
       Some website-created stations may not register on the map. (This is
@@ -48,7 +51,7 @@ const WelcomeView = props => (
       title="Let's Go!"
       onPress={() => props.navigation.navigate("Auth")}
     />
-  </View>
+  </ScrollView>
 );
 
 export default WelcomeView;
