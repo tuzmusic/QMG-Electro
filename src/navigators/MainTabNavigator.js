1b674a082af29091ef329a02276db9973a439448
tuzmusic <tuzmusic@gmail.com>
2019-07-15 11:46:26 -0400

    reactivate fetching

--- src/navigators/MainTabNavigator.js
+++ src/navigators/MainTabNavigator.js
@@ -78,8 +78,8 @@ class TabContainer extends Component {
   }
 
   componentDidMount = async () => {
-    // await this.props.fetchStations();
     // if (!__DEV__) await this.props.fetchStations();
+    await this.props.fetchStations();
   };
 
   static router = TabNavigator.router;
